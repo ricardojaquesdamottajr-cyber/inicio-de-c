c1b2f34d31998716840a8e4f5db5fef008786852
spookybefer <ricardojaquesdamottajr@gmail.com>
2026-02-25 17:30:14 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,36 +1,48 @@
 ﻿using System;
 
-namespace teste_c
-{
+namespace OiEuSouOGoku
+{ //inicio
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //metodo principal
         {
+            // int conta = 1 + 2;
+            // string nome;
+            // nome = "Ricardo";
+            // int idade = 2026 - 2007;
+            // Console.WriteLine("o resultado de 2 + 5 ="+ (2+5)); //comando de imprimir algo na tela
+            //  Console.WriteLine("o resultado de 2 - 5 =" + (2 - 5));
+            // Console.WriteLine("o resultado de 2 * 5 =" + (2 * 5));
+            // Console.WriteLine("o resultado de 2 % 5 =" + (2 * 5 / 100));
+            //  Console.WriteLine(conta);
+            // Console.WriteLine(Math.PI);
+            // Console.WriteLine("meu nome é " + (nome) + " a minha idade é " + (idade));
+            //declaraçao de variavel
+
+            // int  (numerico interiro ->0)
+            // double (numerico real -> 0.00)
+            // bool  (valor logico -> if/else)
+            // char  (uma unica letra -> 's)
+            // string  (cadeia de caracteres -> "oi eu sou lindo")
+
+            //declarar variavel nome e idade , apresentar nome e idade
+
+            // int numero = 4;
+            //int sucessor = numero + 1;
+            //int antecessor = numero - 1;
 
 
-            int idade = 18;
-            double precoproduto = 1.99;
-            char sexo = 'f';
-            string nome = "elias";
-            bool ligado = true;
 
+            //Console.WriteLine("o numero selecionado é " + (numero) + " o seu antecessor é " + (antecessor) + " e o seu sucessor é " + (sucessor));
+            int numero1 = 20;
+            int numero2 = 30;
+            int quadrado2 = numero1 * numero1;
+            int quadrado3 = numero2 * numero2;
+            int soma = quadrado2 + quadrado3;
+  
 
-            Console.WriteLine(idade);
-            Console.WriteLine(precoproduto);
-            Console.WriteLine(nome);
-            Console.WriteLine(ligado);
-            Console.WriteLine(sexo);
+            Console.WriteLine("o quadrado dos numeros sera " + (soma));
 
-
-            Console.WriteLine("ola " + nome);
-            int x = 3;
-            int y = 2;
-            Console.WriteLine(x + y);
-
-
-            Console.WriteLine("digite o nome de usuario");
-            string username = Console.ReadLine();
-            Console.WriteLine("o nome do usuario é " + username);
-        }
+        }          
     }
-}
+} //fim do codigo
