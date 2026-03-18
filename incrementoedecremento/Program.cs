9962ab792f73c7dba5d5163d324d008cdf283617
spookybefer <ricardojaquesdamottajr@gmail.com>
2026-03-18 17:25:23 -0300

    Add files via upload

---
+++ incrementoedecremento/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace incrementoedecremento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine(++i);
+
+            //pos decremento
+
+            Console.WriteLine(i++);
+            Console.WriteLine(i);
+
+            // decremento
+
+            i--;
+            Console.WriteLine(i);
+        }
+    }
+}
