6ce2db390550b31613185fd399236fed67401601
spookybefer <ricardojaquesdamottajr@gmail.com>
2026-03-18 17:32:53 -0300

    Add files via upload

---
+++ operadoresLogicos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace operadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 25;
+            bool temcarteira = true;
+
+            if(idade >= 25 && temcarteira)
+            {
+                Console.WriteLine("Pode Dirigir");
+            }
+
+            bool ehfinaldesemana = false;
+            bool ehferiado = true;
+
+           if (ehfinaldesemana || ehferiado)
+            {
+               Console.WriteLine("Pode descansar");
+           }
+
+           bool usuariologado = false;
+
+            if (!usuariologado)
+            {
+                Console.WriteLine("Por Favor, Fazer Login");
+            }
+        }
+    }
+}
