7910f4bd32406036c00dfdf2b9a849efe0f94b46
spookybefer <ricardojaquesdamottajr@gmail.com>
2026-03-18 17:18:39 -0300

    Add files via upload
    atividade de C# 18/03

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class atividadeC
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 3;
+
+            int resto = a % b;
+
+            Console.WriteLine($"o resto de {a} dividido por {b} e : {resto}");
+        }
+    }
+}
