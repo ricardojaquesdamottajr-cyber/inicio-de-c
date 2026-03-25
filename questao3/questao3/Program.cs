b9ca971d22ea3f2b06eab8c07fcd23283a6f8601
spookybefer <ricardojaquesdamottajr@gmail.com>
2026-03-25 17:44:00 -0300

    Add files via upload

---
+++ questao3/questao3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("informe quantso carros voce vendeu ");
+            double vendascarros = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("informe o valor total das suas vendas ");
+            double valorvendas = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("informe quanto é o seu salario fixo ");
+            double salario = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("quanto voce ganha por carro vendido? ");
+            double carrovendido = Convert.ToDouble(Console.ReadLine());
+
+            double calculovenda = valorvendas * 0.05;
+
+            double calculo = calculovenda + salario + carrovendido;
+
+            Console.WriteLine("o seu salario mfinal sera de: " + calculo);
+        }
+    }
+}
