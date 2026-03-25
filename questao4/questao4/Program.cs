5b69678fab027dbbf6bb082dfe4b98aa6be29d50
spookybefer <ricardojaquesdamottajr@gmail.com>
2026-03-25 17:42:18 -0300

    Add files via upload

---
+++ questao4/questao4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace questao4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("quantas maçãs voce comprou? ");
+            int maças = Convert.ToInt32(Console.ReadLine());
+
+            if (maças < 12)
+            {
+                double calculo = maças * 1.30;
+                Console.WriteLine("voce comprou " + maças + " maças o valor delas sera de " + calculo + "R$");
+            }
+            else
+            {
+                double calculo = maças * 1.00;
+                Console.WriteLine("voce comprou " + maças + " maças o valor delas sera de " + calculo + "R$");
+            }
+                 
+        }
+    }
+}
