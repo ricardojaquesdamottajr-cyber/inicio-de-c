964ed065976a720ba697df303b95807076e35cf2
spookybefer <ricardojaquesdamottajr@gmail.com>
2026-03-25 17:43:31 -0300

    Add files via upload

---
+++ questao2/questao2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("informe o seu salario");
+            double salario = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("quantos % vai ser o reajuste de salario?");
+            double porcentagem = Convert.ToDouble(Console.ReadLine());
+            double calculo = salario * porcentagem / 100.0;
+            double novovalor = salario - calculo;
+            Console.WriteLine("o novo salario sera de: " + novovalor );
+        }
+    }
+}
