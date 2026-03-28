deaa174adb06e8fee60982a1a1c90e68fec6063b
spookybefer <ricardojaquesdamottajr@gmail.com>
2026-03-27 22:23:49 -0300

    alteraçao

--- atividadesC/atividadesC/questao1.cs
+++ atividadesC/atividadesC/questao1.cs
@@ -8,12 +8,12 @@ namespace atividadesC
         {
 
             Console.WriteLine("quantas pessoas irão votar?");
-            int pessoas = Convert.ToInt32(Console.ReadLine());
+            double pessoas = Convert.ToInt32(Console.ReadLine());
            
-            int nulos = 0;
-            int invalidos = 0;
-            int branco = 0;
-            int validos = 0;
+            double nulos = 0;
+            double invalidos = 0;
+            double branco = 0;
+            double validos = 0;
 
             for (int i = 0; i< pessoas;) {
                 Console.WriteLine("voce vai votar? se sim digite 1 se não digite 2 ");
@@ -45,15 +45,15 @@ namespace atividadesC
                     pessoas--;
                 }
             }
-            double calculoporcentagembranco = branco / 100.0;
-            double calculonulosporcentagem = nulos / 100.0;
-            double calculovalidosporcentagem = validos / 100.0;
+            double calculoporcentagembranco = Math.Round( branco / pessoas * 100.0) ;
+            double calculonulosporcentagem = Math.Round  (nulos / pessoas * 100.0);
+            double calculovalidosporcentagem = Math.Round( validos / pessoas * 100.0);
 
             double calculoeleitoresnulos = pessoas * calculonulosporcentagem;
             double calculoeleitoresembranco = pessoas * calculoporcentagembranco;
             double calculovalidoeleitoes = pessoas * calculoporcentagembranco;
 
-            Console.WriteLine("o total de eleitores é " + pessoas + " o numero de votos validos foi " + validos + " a porcentagem de de pessoas que votaram em votos nulos foi " + calculonulosporcentagem + " a porcentagem de votos em branco foi " + calculoporcentagembranco );
+            Console.WriteLine("o total de eleitores é " + pessoas + " o numero de votos validos foi " + validos + " a porcentagem de de pessoas que votaram em votos nulos foi " + calculonulosporcentagem + "% a porcentagem de votos em branco foi " + calculoporcentagembranco + "%" );
         }
     }
 }
