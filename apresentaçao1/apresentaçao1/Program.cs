53d068935b3b807e40f52588c17cc556185ca033
spookybefer <ricardojaquesdamottajr@gmail.com>
2026-04-22 15:24:54 -0300

    Add files via upload

---
+++ apresentaçao1/apresentaçao1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace apresentaçao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("qual é a sua idade");
+            int idade = Convert.ToInt32(Console.ReadLine());
+            if (idade < 18)
+            {
+                Console.WriteLine("Menor de Idade");
+            }
+            else if (idade >= 18 && idade<= 59)
+            {
+                Console.WriteLine("Adulto");
+            }
+            else
+            {
+                Console.WriteLine("idoso");
+            }
+
+        }
+    }
+}
---
+++ apresentaçao2/apresentaçao2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace apresentaçao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite um numero para saber se é par ou impar");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+                string parimpar = (numero % 2 == 0) ? "par" : "impar";
+
+                Console.WriteLine(parimpar);
+            }
+        }
+    }
+
